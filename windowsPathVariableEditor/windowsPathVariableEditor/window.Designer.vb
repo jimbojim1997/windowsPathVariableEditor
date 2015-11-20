28419354aa895cf0afb48cfd44c86cf905119a1d
jimbojim1997 <jimbojim1997@gmail.com>
2015-11-20 16:30:10 +0000

    Initial version with working editor

---
+++ windowsPathVariableEditor/windowsPathVariableEditor/window.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class window
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(window))
+        Me.lbVariables = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnModify = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbVariables
+        '
+        Me.lbVariables.FormattingEnabled = True
+        Me.lbVariables.Location = New System.Drawing.Point(2, 2)
+        Me.lbVariables.Name = "lbVariables"
+        Me.lbVariables.Size = New System.Drawing.Size(328, 459)
+        Me.lbVariables.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Location = New System.Drawing.Point(331, 31)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnModify
+        '
+        Me.btnModify.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnModify.Location = New System.Drawing.Point(331, 60)
+        Me.btnModify.Name = "btnModify"
+        Me.btnModify.Size = New System.Drawing.Size(75, 23)
+        Me.btnModify.TabIndex = 2
+        Me.btnModify.Text = "Modify"
+        Me.btnModify.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.Location = New System.Drawing.Point(331, 89)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.Location = New System.Drawing.Point(331, 2)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 4
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'window
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 463)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnModify)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lbVariables)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "window"
+        Me.Text = "Variable Editor"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lbVariables As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnModify As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+End Class
---
+++ windowsPathVariableEditor/windowsPathVariableEditor/window.vb
@@ -0,0 +1,42 @@
+﻿Public Class window
+    Private Sub window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim paths As String = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.User)
+        For Each path As String In paths.Split(";")
+            If Not String.IsNullOrWhiteSpace(path) Then
+                lbVariables.Items.Add(path)
+            End If
+        Next
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim paths As String = ""
+        For Each path As String In lbVariables.Items
+            paths += path & ";"
+        Next
+        Environment.SetEnvironmentVariable("path", paths, EnvironmentVariableTarget.User)
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim value As String = InputBox("Enter value", "Enter Value")
+        If Not String.IsNullOrWhiteSpace(value) Then
+            lbVariables.Items.Add(value)
+        End If
+    End Sub
+
+    Private Sub btnModify_Click(sender As Object, e As EventArgs) Handles btnModify.Click
+        Dim index As Integer = lbVariables.SelectedIndex
+        If Not IsNothing(index) Then
+            Dim value As String = InputBox("Enter value", "Enter Value", lbVariables.Items.Item(index))
+            If Not String.IsNullOrWhiteSpace(value) Then
+                lbVariables.Items.Item(index) = value
+            End If
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim index As Integer = lbVariables.SelectedIndex
+        If Not IsNothing(index) Then
+            lbVariables.Items.RemoveAt(index)
+        End If
+    End Sub
+End Class
